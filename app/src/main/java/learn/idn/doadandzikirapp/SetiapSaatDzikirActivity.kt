0f076f9c8ae37eaed685f6bcbe991bb99a159955
akkbarrr <rayhanpro6@gmail.com>
2021-10-20 11:03:31 +0700

    sugoi sugoi

---
+++ app/src/main/java/learn/idn/doadandzikirapp/SetiapSaatDzikirActivity.kt
@@ -0,0 +1,11 @@
+package learn.idn.doadandzikirapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SetiapSaatDzikirActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setiap_saat_dzikir)
+    }
+}
---
+++ app/src/main/java/learn/idn/doadandzikirapp/SplashScreenActivity.kt
@@ -0,0 +1,11 @@
+package learn.idn.doadandzikirapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
